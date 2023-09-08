2ee7a602aad3a4a308bf3682b84384f96a0cd470
Grimmjow-Jaegerjaquez <109174297+Grimmjow-Jaegerjaquez@users.noreply.github.com>
2023-09-08 10:49:21 +0530

    Add files via upload

--- 210962090/midsem/seg.py
+++ 210962090/midsem/seg.py
@@ -7,43 +7,39 @@ image = cv2.imread('resources\\image (19).jpg')
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Apply Gaussian blur to reduce noise and improve edge detection
-blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+# Flatten the grayscale image to a 1D array
+pixels = gray_image.flatten().reshape((-1, 1))
 
-# Use Canny edge detection to find edges in the image
-edges = cv2.Canny(blurred_image, 50, 150)
+# Define the number of clusters (K-means)
+num_clusters = 10  # Adjust this based on your image and desired number of clusters
 
-# Find contours in the edge-detected image
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Apply K-means clustering
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+_, labels, centers = cv2.kmeans(pixels.astype(np.float32), num_clusters, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# Create a copy of the original image for drawing contours
-contour_image = image.copy()
+# Reshape the labels to match the original image shape
+segmented_image = labels.reshape(image.shape[:2])
 
-# Define a list to store detected lesions
-lesions = []
+# Define a threshold to identify potential lesion regions
+lesion_label = 0  # You may need to adjust this label based on your K-means results
 
-# Iterate through the detected contours
+# Create a mask for potential lesion regions
+lesion_mask = (segmented_image == lesion_label).astype(np.uint8)
+
+# Apply post-processing to refine the lesion mask (e.g., morphological operations)
+
+# Find contours in the lesion mask
+contours, _ = cv2.findContours(lesion_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Create a copy of the original image for visualization
+result_image = image.copy()
+
+# Draw bounding boxes around detected lesions
 for contour in contours:
-    # Calculate the area of each contour
-    area = cv2.contourArea(contour)
-
-    # Set a minimum threshold for lesion size
-    min_lesion_area = 5  # Adjust this threshold as needed
-    max_lession_area = 50
-
-    if area > min_lesion_area and area < max_lession_area:
-        # Draw the contour on the original image
-        cv2.drawContours(contour_image, [contour], -1, (0, 255, 0), 2)
-
-        # Get the coordinates and size of a bounding rectangle around the lesion
-        x, y, w, h = cv2.boundingRect(contour)
-
-        # Add the lesion coordinates and size to the list
-        lesions.append((x, y, w, h))
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(result_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # Display the image with detected lesions
-cv2.imshow('Image with Lesions', contour_image)
+cv2.imshow('Image with Lesions', result_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
